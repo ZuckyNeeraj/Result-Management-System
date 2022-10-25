a13bd291accd1566c834483cc377c8db5494dbca
ZuckyNeeraj <zuckyneeraj@gmail.com>
2022-10-25 21:12:47 +0530

    Completed the project

---
+++ src/ManagementSystem/JOPtionPane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ManagementSystem;
+
+/**
+ *
+ * @author zucky
+ */
+class JOPtionPane {
+
+    static void showMessageDialog(Object object, String enetered_Roll_Number_is_wrong) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
